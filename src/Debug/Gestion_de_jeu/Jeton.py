fd0a6d7833f1c53d05b818c33b07d7994b8d9985
theophiIe <theomolinatti@gmail.com>
2021-03-25 11:09:04 +0100

    Création de la classe jeton et de certaines méthodes

---
+++ src/Debug/Gestion_de_jeu/Jeton.py
@@ -0,0 +1,19 @@
+ROUGE = 0
+JAUNE = 1
+
+class Jeton:
+    """
+        Commentaire sur la classe
+    """
+    
+    nombre_jeton = 0
+
+    def __init__(self, j_couleur):
+        """ Commentaire sur la méthode """
+        self.couleur = j_couleur
+
+    def incremente_nombre_jeton(cls):
+        """ Commentaire sur la méthode """
+        cls.nombre_jeton += 1
+
+    incremente_nombre_jeton = classmethod(incremente_nombre_jeton)
