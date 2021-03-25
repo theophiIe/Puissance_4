ed46a7a67c58165e315637abfd64eb63ba4f6db8
theophiIe <theomolinatti@gmail.com>
2021-03-25 11:10:34 +0100

    Creation des méthodes pour le chargement

---
+++ src/Debug/Sauvegarde_et_Chargement/Chargement.py
@@ -0,0 +1,12 @@
+
+def test_corruption(nom_fichier):
+    """ Commentaire sur la fonction """
+    pass
+
+def lecture_fichier(nom_fichier):
+    """ Commentaire sur la fonction """
+    pass
+
+def chargement(nom_fichier):
+    """ Commentaire sur la fonction """
+    pass
