6dd662fdd014de7739bce1ac5ff7868d21496d79
theophiIe <theomolinatti@gmail.com>
2021-03-25 11:10:21 +0100

    Creation des méthodes pour la sauvegarde

---
+++ src/Debug/Sauvegarde_et_Chargement/Sauvegarde.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+def information_sauvegarde():
+    """ Commentaire sur la fonction """
+    pass
+
+def verifie_nom_fichier(nom_fichier):
+    """ Commentaire sur la fonction """
+    pass
+
+def supprimer_sauvegarde(nom_fichier):
+    """ Commentaire sur la fonction """
+    pass
+
+def sauvegarde(grille, nom_fichier):
+    """ Commentaire sur la fonction """
+    pass
