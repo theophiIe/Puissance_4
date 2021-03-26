d6006f343f4f64d2e53aaf952da0bbd4ab367994
theophiIe <theomolinatti@gmail.com>
2021-03-26 15:43:01 +0100

    Test jouer un coup pas fonctionnel

--- src/Debug/Gestion_de_jeu/Joueur.py
+++ src/Debug/Gestion_de_jeu/Joueur.py
@@ -1,3 +1,5 @@
+import Jeton
+import Grille
 
 class Joueur:
     """
@@ -8,16 +10,32 @@ class Joueur:
         """ Commentaire sur le constructeur """
         self.commence = j_commence
 
+    def jouer_coup(self, grilles, j_num_colonne):
+        """Commentaire"""
+        jeton = Jeton.Jetons(0)
+        Jeton.Jetons.incremente_nombre_jeton()
+        grilles[0][j_num_colonne] = jeton
+        
+
 class Ordinateur(Joueur):
     """
-        Commentaire sur la classe
+        Commentaire sur la classe (classe fille de Joueur)
     """
 
     def __init__(self, o_commence, o_difficulte):
-        """
-            Constructeur de la classe Ordinateur
-            Fait appel au constrcuteur de la classe Joueur
-            et permet d'assigner un niveau de difficulté à l'ordinateur
-        """
+        """ Commentaire sur le constructeur """
         Joueur.__init__(o_commence)
         self.difficulte = o_difficulte
+
+    def premier_coup(self):
+        """ Commentaire """
+        pass
+
+
+board = Grille.Grilles(7,6)
+print(board)
+
+j1 = Joueur(1)
+j1.jouer_coup(board, 0)
+
+print(board)
