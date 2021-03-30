7f8245c9083b8d64834aeec6d977857fa12ee056
VirarK <43572288+VirarK@users.noreply.github.com>
2021-03-30 16:41:05 +0200

    Update Sauvegarde.py
    modif import + fautes

--- src/Debug/Sauvegarde_et_Chargement/Sauvegarde.py
+++ src/Debug/Sauvegarde_et_Chargement/Sauvegarde.py
@@ -1,15 +1,16 @@
 import os
 import time
+import Gestion_grille
 
 def information_sauvegarde():
     """
-        Cette fonction permet de récuperer le nom
+        Cette fonction permet de récupérer le nom
         des fichiers correspondant aux sauvegardes
         des parties.
 
         Cette fonction renvoie un tableau contenant les 
-        noms des fichiers de sauvegardes sous forme de 
-        chaines de caractères.
+        noms des fichiers de sauvegarde sous forme de 
+        chaînes de caractères.
     """
     pass
 
@@ -17,7 +18,7 @@ def verification_syntaxe_nom_fichier(nom_fichier):
     """
         Cette fonction permet de vérifier que le nom
         choisi pour le fichier de sauvegarde est syntaxiquement
-        correcte.
+        correct.
 
         Paramètre:
             nom_fichier : variable de type chaine de caractères correspondant
@@ -35,12 +36,12 @@ def verifie_existance_fichier(nom_fichier):
         ayant pour nom le nom donné en argument existe déjà.
 
         Paramètre:
-            nom_fichier : variable de type chaine de caractères correspondant
+            nom_fichier : variable de type chaîne de caractères correspondant
                             au nom du fichier.
 
         Cette fonction renvoie un booléen :
-            True  : si la syntaxe est correcte.
-            False : si la syntaxe est incorrecte.
+            True  : si le nom de fichier est indisponible.
+            False : si le nom de fichier est disponible.
     """
     pass
 
@@ -50,28 +51,28 @@ def supprimer_sauvegarde(nom_fichier):
         de sauvegarde qui a pour nom celui donné en argument.
 
         Paramètre:
-            nom_fichier : variable de type chaine de caractères correspondant
+            nom_fichier : variable de type chaîne de caractères correspondant
                             au nom du fichier.
 
         Cette fonction renvoie un booléen :
-            True  : si la suppression a été correctement effectué.
+            True  : si la suppression a été correctement effectuée.
             False : si la suppression a rencontré un problème.
     """
     pass
 
 def sauvegarde(grille, nom_fichier):
     """
-        Cette fonction permet, à partir des informations de la grille passé en paramètre,
-        de sauvegarder cette dernière dans un fichier texte qui à pour nom celui passé en paramètre,
+        Cette fonction permet, à partir des informations de la grille passée en paramètre,
+        de sauvegarder cette dernière dans un fichier texte qui a pour nom celui passé en paramètre,
         s'il n'existe pas déjà.
 
         Paramètres:
-            grille : matrice de la classe Grilles.
-            nom_fichier : variable de type chaine de caractères correspondant
+            grille : matrice de la classe Grille.
+            nom_fichier : variable de type chaîne de caractères correspondant
                             au nom du fichier.
 
         Cette fonction renvoie un booléen :
-            True  : si la sauvegarde a été correctement effectué.
+            True  : si la sauvegarde a été correctement effectuée.
             False : si la sauvegarde a rencontré un problème.
     """
     pass
