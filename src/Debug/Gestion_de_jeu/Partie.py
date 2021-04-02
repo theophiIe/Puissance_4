b5fbf8b2e46ab260524c8677f6631b6b403c6b0f
wapitix <44016998+wapitix@users.noreply.github.com>
2021-04-02 13:50:41 +0100

    Update Partie.py
    Maj paramètre de fin_partie()

--- src/Debug/Gestion_de_jeu/Partie.py
+++ src/Debug/Gestion_de_jeu/Partie.py
@@ -1,11 +1,14 @@
 import Gestion_grille
 import Gestion_joueur
 
-def fin_de_partie():
+def fin_de_partie(cls_grille):
     """
         Cette fonction permet de vérifier la potentielle fin de partie
         match nul ou un gagnant (en appelant les méthodes coup_gagnant et est_pleine de la classe Grille).
 
+        Paramètre : 
+            cls_grille : instance de la classe Grille
+            
         Cette fonction renvoie un entier.
         2 si un coup gagnant est joué, 1 si la grille est pleine sinon 0.
     """
