6f02f70fa3fbcb6adf45765525014b873660697f
wapitix <44016998+wapitix@users.noreply.github.com>
2021-04-10 15:55:16 +0100

    update 2 commentaires

--- src/Debug/Interface_Graphique/Gestion_interface.py
+++ src/Debug/Interface_Graphique/Gestion_interface.py
@@ -11,13 +11,13 @@ class Bouton:
             
             Paramètres :
                 b_texte : chaine de caractère affichée dans le bouton
-                b_couleur_texte : couleur du texte affiché dans le bouton
-                b_couleur_jeton : coukeur du jeton
-                b_taille_texte : 
-                b_position_x :
-                b_position_y :
-                b_largeur :
-                b_hauteur :
+                b_couleur_texte : chaine de caractère qui correspond à la couleur du texte affiché dans le bouton
+                b_couleur_jeton : chaine de caractère qui correspond couleur du jeton
+                b_taille_texte : entier pour la taille du texte affiché
+                b_position_x : entier pour la position en abcisse du bouton
+                b_position_y : entier pour la position en ordonées du bouton
+                b_largeur : entier pour la largeur du bouton
+                b_hauteur : entier pour la longueur du bouton
                 
             
         """
@@ -25,7 +25,7 @@ class Bouton:
     
     
     def affichage_bouton(self, b_fenetre):
-        """ Méthode de classe permettant l'affichage d'un bouton dans le fenêtre graphique 
+        """ Méthode de classe permettant l'affichage d'un bouton dans la fenêtre graphique 
             
             Paramètres :
                 b_fenetre : 
@@ -34,20 +34,20 @@ class Bouton:
         pass
 
     def changement_taille_bouton(self, b_taille_texte, b_position_x, b_position_y, b_largeur, b_hauteur):
-        """ Méthode de classe de modifier la taille, la police et la position d'un bouton 
+        """ Méthode de classe qui permet de modifier la taille, la police et la position d'un bouton 
             
             Paramètre : 
-            b_taille_texte : 
-            b_position_x : 
-            b_position_y : 
-            b_largeur : 
-            b_hauteur : 
+                b_taille_texte : entier pour la taille du texte affiché
+                b_position_x : entier pour la position en abcisse du bouton
+                b_position_y : entier pour la position en ordonée du bouton
+                b_largeur : entier pour la largeur du bouton
+                b_hauteur : entier pour la longueur du bouton
            
         """
         pass
     
-    def cacher_bouton(self,):
-    """ Méthode de classe de modifier la taille, la police et la position d'un bouton 
+    def cacher_bouton(self):
+    """ Méthode de classe qui permet de cacher un bouton déjà affiché dans l'interface graphique
 
         Paramètre : 
          
