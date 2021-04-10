ccc2f95b08817bd10d4d1d93ba50b301a70db54f
wapitix <44016998+wapitix@users.noreply.github.com>
2021-04-10 16:25:58 +0100

    Update de la gestion de l'affichage des fenetres

--- src/Debug/Interface_Graphique/Gestion_interface.py
+++ src/Debug/Interface_Graphique/Gestion_interface.py
@@ -1,44 +1,70 @@
 import pygame
 
-def affichage_menu_principal(fenetre):
+def affichage_menu_principal(fenetre):  #cas 0
     """
         Fonction pour l'affichage du menu principal
     """
+    pass
    
-def affichage_mode_de_jeu(fenetre):
+def affichage_mode_de_jeu(fenetre, mode_de_jeu):    #cas 1
     """
         Fonction pour l'affichage du choix du mode de jeu
     """
-    
-def affichage_choix_de_difficulte(fenetre):
+    pass
+
+def affichage_choix_de_difficulte(fenetre): #cas 2
     """
         Fonction pour l'affichage du choix de difficulté
     """
-    
-def affichage_partie(fenetre):
+    pass
+def affichage_partie(fenetre):  #cas 3
     """
         Fonction pour l'affichage de la partie
     """
-    
-def affichage_sauvegarde(fenetre):
+    pass
+
+def affichage_sauvegarde(fenetre):  #cas 4
     """
         Fonction pour l'affichage de la partie sauvegarde
     """
-   
-def affichage_chargement(fenetre):
+    pass
+
+def affichage_nouvelle_sauvegarde(fenetre): #cas 5
+    """
+        Fonction pour l'affichage de la partie sauvegarde
+    """
+    pass
+
+def affichage_chargement(fenetre):  #cas 6
     """
         Fonction pour l'affichage de la partie de chargement
     """
-def affichage_validation(fenetre):
+    pass
+
+def affichage_victoire(fenetre):    #cas 7
+    """
+        Fonction pour l'affichage de fin de partie avec la victoire
+    """
+    pass
+
+def affichage_validation(fenetre):  #cas 8
     """
         Fonction pour l'affichage des validations
     """
-    
-def affichage_erreur(fenetre):
+    pass    
+def affichage_commencer(fenetre):   #cas 9
     """
-        Fonction pour l'affichage 
+        Fonction pour l'affichage pour le joueur qui commence à jouer
     """
+    pass
     
+def affichage_erreur(fenetre):  #cas 10
+    """
+        Fonction pour l'affichage des erreurs
+    """
+    pass
+    
+
 
   
     
