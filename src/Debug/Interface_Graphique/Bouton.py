8e129a21868ca49d1908d7bd8c47b161cdee070e
wapitix <44016998+wapitix@users.noreply.github.com>
2021-04-10 15:59:59 +0100

    Ajout fichier pour le Bouton

---
+++ src/Debug/Interface_Graphique/Bouton.py
@@ -0,0 +1,70 @@
+import pygame
+
+class Bouton:
+    """
+        Classe Bouton
+    """
+
+    def __init__(self, b_texte, b_couleur_texte, b_couleur_bouton, b_taille_texte, b_position_x, b_position_y, b_largeur, b_hauteur):
+        """ Constructeur de la classe Bouton
+            Permet de créer un nouveau bouton qui sera affiché dans l'interface graphique.
+            
+            Paramètres :
+                b_texte : chaine de caractère affichée dans le bouton
+                b_couleur_texte : chaine de caractère qui correspond à la couleur du texte affiché dans le bouton
+                b_couleur_jeton : chaine de caractère qui correspond couleur du jeton
+                b_taille_texte : entier pour la taille du texte affiché
+                b_position_x : entier pour la position en abcisse du bouton
+                b_position_y : entier pour la position en ordonées du bouton
+                b_largeur : entier pour la largeur du bouton
+                b_hauteur : entier pour la longueur du bouton
+                
+            
+        """
+        pass
+    
+    
+    def affichage_bouton(self, b_fenetre):
+        """ Méthode de classe permettant l'affichage d'un bouton dans la fenêtre graphique 
+            
+            Paramètres :
+                b_fenetre : 
+        """
+            
+        pass
+
+    def changement_taille_bouton(self, b_taille_texte, b_position_x, b_position_y, b_largeur, b_hauteur):
+        """ Méthode de classe qui permet de modifier la taille, la police et la position d'un bouton 
+            
+            Paramètre : 
+                b_taille_texte : entier pour la taille du texte affiché
+                b_position_x : entier pour la position en abcisse du bouton
+                b_position_y : entier pour la position en ordonée du bouton
+                b_largeur : entier pour la largeur du bouton
+                b_hauteur : entier pour la longueur du bouton
+           
+        """
+        pass
+    
+    def cacher_bouton(self):
+    """ Méthode de classe qui permet de cacher un bouton déjà affiché dans l'interface graphique
+
+        Paramètre : 
+         
+    """
+    pass
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
