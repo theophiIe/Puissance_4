fcc716a9d49174a12f2c4e87f5482eac1e633574
wapitix <44016998+wapitix@users.noreply.github.com>
2021-04-10 15:05:42 +0100

    v1 Commentaires + Fonctions de l'interface

--- src/Debug/Interface_Graphique/Gestion_interface.py
+++ src/Debug/Interface_Graphique/Gestion_interface.py
@@ -1,11 +1,70 @@
 import pygame
 
-class Interface:
+class Bouton:
     """
-        Classe Interface
+        Classe Bouton
     """
 
-    def __init__(self):
+    def __init__(self, b_texte, b_couleur_texte, b_couleur_bouton, b_taille_texte, b_position_x, b_position_y, b_largeur, b_hauteur):
+        """ Constructeur de la classe Bouton
+            Permet de créer un nouveau bouton qui sera affiché dans l'interface graphique.
+            
+            Paramètres :
+                b_texte : chaine de caractère affichée dans le bouton
+                b_couleur_texte : couleur du texte affiché dans le bouton
+                b_couleur_jeton : coukeur du jeton
+                b_taille_texte : 
+                b_position_x :
+                b_position_y :
+                b_largeur :
+                b_hauteur :
+                
+            
+        """
         pass
+    
+    
+    def affichage_bouton(self, b_fenetre):
+        """ Méthode de classe permettant l'affichage d'un bouton dans le fenêtre graphique 
+            
+            Paramètres :
+                b_fenetre : 
+        """
+            
+        pass
+
+    def changement_taille_bouton(self, b_taille_texte, b_position_x, b_position_y, b_largeur, b_hauteur):
+        """ Méthode de classe de modifier la taille, la police et la position d'un bouton 
+            
+            Paramètre : 
+            b_taille_texte : 
+            b_position_x : 
+            b_position_y : 
+            b_largeur : 
+            b_hauteur : 
+           
+        """
+        pass
+    
+    def cacher_bouton(self,):
+    """ Méthode de classe de modifier la taille, la police et la position d'un bouton 
+
+        Paramètre : 
+         
+    """
+    pass
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
