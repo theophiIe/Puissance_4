b6549b3f92a0baa59335b8a0a2b36e224cd9e215
MisterCHOUN <65115291+MisterCHOUN@users.noreply.github.com>
2021-04-12 18:10:03 +0200

    nouvelle classe

--- src/Debug/Interface_Graphique/Gestion_bouton.py
+++ src/Debug/Interface_Graphique/Gestion_bouton.py
@@ -1,70 +1,48 @@
 import pygame
 
 class Bouton:
-    """
-        Classe Bouton
-    """
+    def __init__(self, b_image, b_image_survole, b_position_x, b_position_y, b_largeur, b_hauteur):
+        
+        self.point_x = b_position_x
+        self.point_y = b_position_y
 
-    def __init__(self, b_texte, b_couleur_texte, b_couleur_bouton, b_taille_texte, b_position_x, b_position_y, b_largeur, b_hauteur):
-        """ Constructeur de la classe Bouton
-            Permet de créer un nouveau bouton qui sera affiché dans l'interface graphique.
-            
-            Paramètres :
-                b_texte : chaine de caractère affichée dans le bouton
-                b_couleur_texte : chaine de caractère qui correspond à la couleur du texte affiché dans le bouton
-                b_couleur_jeton : chaine de caractère qui correspond couleur du jeton
-                b_taille_texte : entier pour la taille du texte affiché
-                b_position_x : entier pour la position en abcisse du bouton
-                b_position_y : entier pour la position en ordonées du bouton
-                b_largeur : entier pour la largeur du bouton
-                b_hauteur : entier pour la longueur du bouton
-                
-            
-        """
-        pass
-    
-    
+        self.rectangle = pygame.Rect(b_position_x, b_position_y, b_largeur, b_hauteur)
+
+        self.chargement_image = pygame.image.load(b_image)
+        self.image = pygame.transform.scale(self.chargement_image,(int(b_largeur), int(b_hauteur)))
+        
+        self.chargement_image_survole = pygame.image.load(b_image_survole)
+        self.image_survole = pygame.transform.scale(self.chargement_image_survole,(int(b_largeur), int(b_hauteur)))
+
+
+    def changement_taille_bouton(self, b_position_x, b_position_y, b_largeur, b_hauteur):
+        
+        self.point_x = b_position_x
+        self.point_y = b_position_y
+
+        self.rectangle = pygame.Rect(b_position_x, b_position_y, b_largeur, b_hauteur)
+
+        self.image = pygame.transform.scale(self.chargement_image,(int(b_largeur), int(b_hauteur)))
+        
+        self.image_survole = pygame.transform.scale(self.chargement_image_survole,(int(b_largeur), int(b_hauteur)))
+
+
     def affichage_bouton(self, b_fenetre):
-        """ Méthode de classe permettant l'affichage d'un bouton dans la fenêtre graphique 
-            
-            Paramètres :
-                b_fenetre : 
-        """
-            
-        pass
+        WHITE = pygame.Color("white")
+        pygame.draw.rect(b_fenetre, WHITE, self.rectangle, 1)
+        b_fenetre.blit(self.image, (self.point_x, self.point_y) )
 
-    def changement_taille_bouton(self, b_taille_texte, b_position_x, b_position_y, b_largeur, b_hauteur):
-        """ Méthode de classe qui permet de modifier la taille, la police et la position d'un bouton 
-            
-            Paramètre : 
-                b_taille_texte : entier pour la taille du texte affiché
-                b_position_x : entier pour la position en abcisse du bouton
-                b_position_y : entier pour la position en ordonée du bouton
-                b_largeur : entier pour la largeur du bouton
-                b_hauteur : entier pour la longueur du bouton
-           
-        """
-        pass
-    
-    def cacher_bouton(self):
-    """ Méthode de classe qui permet de cacher un bouton déjà affiché dans l'interface graphique
+    def affichage_bouton_survole(self, b_fenetre):
+        b_fenetre.blit(self.image_survole, (self.point_x, self.point_y) )
 
-        Paramètre : 
-         
-    """
-    pass
+    def collision_bouton(self, b_fenetre, position_souris):
+        collision = self.rectangle.collidepoint(position_souris)
+
+        if collision:
+            self.affichage_bouton_survole(b_fenetre)
+        else:
+            self.affichage_bouton(b_fenetre)
 
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     
