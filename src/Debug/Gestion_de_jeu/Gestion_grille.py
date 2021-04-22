0eec944b6b835782faf3fc1525ccfca12c78e83f
LouisWood <46609485+LouisWood@users.noreply.github.com>
2021-04-22 18:43:35 +0200

    Modification de Gestion_grille.py

--- src/Debug/Gestion_de_jeu/Gestion_grille.py
+++ src/Debug/Gestion_de_jeu/Gestion_grille.py
@@ -1,63 +1,22 @@
 import numpy as ny
-import Puissance_4/src/Gestion_de_Jeu/Gestion_jeton
+import Puissance_4/src/Gestion_de_Jeu/Gestion_jeton as gj
 
 class Grille:
-    """
-        Classe Grille permet de gérer la grille de jeu
-        cette grille est représentée par une matrice contenant
-        des objets de type Jeton ou objet none s'il n'y a pas de Jeton 
-    """
 
     def __init__(self, g_ligne, g_colonne):
-        """
-            Constructeur de la classe Grille.
-            Ce constructeur permet d'initialiser un matrice vide de type object
-            de taille g_longueur par g_largeur.
-            Paramètre :
-                g_ligne   : nombre de lignes de la grille de type entier positif
-                g_colonne : nombre de colonnes de la grille de type entier positif
-        """
         self.grille = ny.empty((g_ligne, g_colonne), dtype=object)
+        self.ligne = g_ligne
+        self.colonne = g_colonne
 
     def coup_valide(self, g_num_colonne):
-        """
-            Cette méthode permet de vérifier si le coup est valide ou non. 
-            Pour cela, on regardera si la colonne où le coup
-            doit être joué est pleine.
-            Paramètre:
-                g_num_colonne : correspond au numéro de colonne où le coup veut être joué
-                                le paramètre est de type entier
-            Renvoie: bool
-                true si le coup est valide
-                false si le coup n'est pas valide
-        """
-        if(ny.size(self.grille) == 0):
-            return False
-
-        if(self.grille[0][g_num_colonne] is None):
-            return True
-
-        return False
+        return (self.grille[0][g_num_colonne] is None)
 
     def coup_gagnant(self, g_num_colonne):
-        """
-            Cette méthode permet vérifier si le coup qui vient d'être joué
-            est un coup gagnant ou non en vérifiant si 4 jetons de la même
-            couleur sont alignés.
-            Paramètre:
-                g_num_colonne : correspond au numéro de colonne où le coup a été joué
-                                le paramètre est de type entier
-            Renvoie: bool
-                true si on a un quadruplet de jetons de la même couleur
-                false si le coup n'est pas gagnant
-        """
-        if(ny.size(self.grille) == 0):
-            return False
-
         num_ligne = 5
-        for i in range(0,6):
+        for i in range(0,self.ligne):
             if(self.grille[i][g_num_colonne] is not None):
                 num_ligne = i
+                break
         
         for i in range(-3,1):
             ligne = num_ligne + i
@@ -103,41 +62,23 @@ class Grille:
         return False
     
     def est_pleine(self):
-        """
-           Cette méthode permet de vérifier si la grille de jeu est pleine.
-           Cette méthode renvoie un booléen.
-           True si la grille est pleine, False sinon.
-        """
-        if(Jeton.nombre_jeton >= 42):
-            return True
-        return False
+        return (gj.Jeton.nombre_jeton == (self.ligne*self.colonne))
 
     def vider_grille(self):
-        """
-            Cette méthode permet de vider la grille
-            en remettant toutes les cases de la matrice
-            à none.
-        """
-        for i in range(6):
-            for j in range(7):
+        for i in range(self.ligne):
+            for j in range(self.colonne):
                 if(self.grille[i][j] is not None):
                     self.grille[i][j] = None
         pass
     
     def remplir_grille(self, g_contenu_grille):
-        """
-            Cette méthode permet de remplir la grille
-            grâce à la chaîne de caractères passée en argument.
-            
-            Cette méthode ne renvoie rien.
-        """
         i = 0
         j = 0
         for jeton in g_contenu_grille:
             if(jeton != '0'):
-                self.grille[i][j] = Jeton(jeton)
+                self.grille[i][j] = gj.Jeton(jeton)
             j += 1
-            if(j%7 == 0):
-                j = j%7
+            if(j%self.colonne == 0):
+                j = 0
                 i += 1
         pass
