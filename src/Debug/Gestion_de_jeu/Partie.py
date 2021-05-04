e84941b84db47dc0f07e0d836b93231e88c63a69
Lola Pires Pinto <lolapinto@live.fr>
2021-05-04 14:40:57 +0200

    Update Partie.py

--- src/Debug/Gestion_de_jeu/Partie.py
+++ src/Debug/Gestion_de_jeu/Partie.py
@@ -68,7 +68,7 @@ def actions_coup_joueur(grille, num_colonne, joueur_actuel, joueur_suivant, nive
             num_colonne = -1
             num_ligne = -1
     else: 
-        "num_ligne?, num_colonne = fail_soft(grille,joueur_actuel,joueur_suivant,4,-math.inf,math.inf)"
+        "num_colonne = fail_soft(grille,joueur_actuel,joueur_suivant,4,-math.inf,math.inf)"
         num_ligne = joueur_actuel.jouer_coup(grille,num_colonne)
         joueur_actuel,joueur_suivant = joueur_suivant,joueur_actuel
     
