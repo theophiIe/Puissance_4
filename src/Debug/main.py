1750d8c45921afaec77e622cc07235e5a8c4dc08
LouisWood <la.sottas@gmail.com>
2021-05-21 08:33:51 -0400

    Création du fichier main.py

---
+++ src/Debug/main.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('Gestion_de_jeu')
+sys.path.append('Interface_Graphique')
+sys.path.append('Liste_sauvegardes')
+sys.path.append('Sauvegarde_et_Chargement')
+sys.path.append('Strategies')
+
+from Gestion_interface import *
+
+lancer_affichage()
