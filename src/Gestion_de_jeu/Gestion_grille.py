7e535f05b0383746cdb9a3547260591343e4b806
theophiIe <theomolinatti@gmail.com>
2021-05-24 22:39:19 +0200

    Indentation du code

--- src/Gestion_de_jeu/Gestion_grille.py
+++ src/Gestion_de_jeu/Gestion_grille.py
@@ -18,6 +18,7 @@ class Grille:
                 jeton2 = self.grille[ligne][colonne+1]
                 jeton3 = self.grille[ligne][colonne+2]
                 jeton4 = self.grille[ligne][colonne+3]
+
                 if((jeton1 is not None) and (jeton2 is not None) and (jeton3 is not None) and (jeton4 is not None)):
                     if(jeton1.couleur == jeton2.couleur == jeton3.couleur == jeton4.couleur == couleur):
                         return True
@@ -28,6 +29,7 @@ class Grille:
                 jeton2 = self.grille[ligne+1][colonne]
                 jeton3 = self.grille[ligne+2][colonne]
                 jeton4 = self.grille[ligne+3][colonne]
+
                 if((jeton1 is not None) and (jeton2 is not None) and (jeton3 is not None) and (jeton4 is not None)):
                     if(jeton1.couleur == jeton2.couleur == jeton3.couleur == jeton4.couleur == couleur):
                         return True
@@ -38,6 +40,7 @@ class Grille:
                 jeton2 = self.grille[ligne+1][colonne+1]
                 jeton3 = self.grille[ligne+2][colonne+2]
                 jeton4 = self.grille[ligne+3][colonne+3]    
+
                 if((jeton1 is not None) and (jeton2 is not None) and (jeton3 is not None) and (jeton4 is not None)):
                     if(jeton1.couleur == jeton2.couleur == jeton3.couleur == jeton4.couleur == couleur):
                         return True
@@ -48,6 +51,7 @@ class Grille:
                 jeton2 = self.grille[ligne-1][colonne+1]
                 jeton3 = self.grille[ligne-2][colonne+2]
                 jeton4 = self.grille[ligne-3][colonne+3]    
+
                 if((jeton1 is not None) and (jeton2 is not None) and (jeton3 is not None) and (jeton4 is not None)):
                     if(jeton1.couleur == jeton2.couleur == jeton3.couleur == jeton4.couleur == couleur):
                         return True
@@ -62,16 +66,16 @@ class Grille:
             for j in range(self.colonne):
                 if(self.grille[i][j] is not None):
                     self.grille[i][j] = None
-        pass
     
     def remplir_grille(self, g_contenu_grille):
         i = 0
         j = 0
+
         for jeton in g_contenu_grille:
             if(jeton != '0'):
                 self.grille[i][j] = Gestion_jeton.Jeton(int(jeton))
             j += 1
+
             if(j%self.colonne == 0):
                 j = 0
                 i += 1
-        pass
