b42c68e3458f6d448360cad9e5205cc1a3d68ba4
uvsq21800425 <louis---albert.sottas@ens.uvsq.fr>
2021-05-25 12:31:10 +0200

    Ajout/Modif des scripts sous Linux et Mac

---
+++ scripts/setupLinux.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get install python3-pip
+pip3 install pygame
+pip3 install numpy
---
+++ scripts/setupMac.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+brew install python3
+pip3 install pygame=2.0.0.dev6
+pip3 install numpy
